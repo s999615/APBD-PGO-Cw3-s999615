c1900ddab81ef17c421ae2a289e55aeec4fd8d8a
s999615 <s999615@pjwstk.edu.pl>
2026-04-27 00:42:49 +0200

    dodane i zmodyfikowane metodyw class ProduktMenu

--- src/ProduktMenu.java
+++ src/ProduktMenu.java
@@ -11,6 +11,8 @@ public class ProduktMenu {
         this.nazwa = nazwa;
         this.cena = cena;
         this.kategoria = kategoria;
+
+        liczbaProduktow++;
     }
 
     public String getKod() {
@@ -44,12 +46,29 @@ public class ProduktMenu {
     public void setKategoria(String kategoria) {
         this.kategoria = kategoria;
     }
+    
 
-    public static int getLiczbaProduktow() {
+    public static int pokazLiczbaProduktow() {
         return liczbaProduktow;
     }
 
-    public static void setLiczbaProduktow(int liczbaProduktow) {
-        ProduktMenu.liczbaProduktow = liczbaProduktow;
+    @Override
+    public String toString() {
+        return "Kod: " + kod + "Nazwa: " + nazwa + "Cena: " + cena + " zl " + "Kategoria: " + kategoria;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Krok 1: Czy porównuję obiekt z samym sobą?
+        if (this == obj) return true;
+
+        // Krok 2: Czy obj jest null lub innego typu?
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        // Krok 3: Rzutowanie — wiemy już, że obj jest typu Student
+        ProduktMenu other = (ProduktMenu) obj;
+
+        // Krok 4: Porównanie pól decydujących o "równości"
+        return this.kod.equals(other.kod);
     }
 }
